3f4f7449601602a76b6881d331d2319c676279f7
Joshua Haynes <qhs725@mocs.utc.edu>
2016-02-27 14:21:54 -0500

    Adding Organizations section to user_groups.ejs block, making it responsive for mobile devices.

--- public/js/partials/code-entry.js
+++ public/js/partials/code-entry.js
@@ -4,11 +4,11 @@
 $( document ).ready(function() {
     $(".code_btn").click(function () {
 
-        $(".code-panel").removeClass("panel-success panel-danger panel-warning").addClass("panel-default");
+        $(".code-panel").removeClass("panel-success panel-danger panel-warning").addClass("panel-primary");
         $("#code-msg").remove();
 
         if($("#code_insert").val() == ""){
-            $('.code-panel').removeClass("panel-default").addClass("panel-warning");
+            $('.code-panel').removeClass("panel-primary").addClass("panel-warning");
             $('.code-panel .panel-body').append('<p id="code-msg" class"text-warning">Please enter a valid code</p>');
         }
         else{
@@ -18,13 +18,13 @@ $( document ).ready(function() {
             data: $('#code_form').serialize(),
             success: function (data) {
                 $('#code_form')[0].reset();
-                $('.code-panel').removeClass("panel-default").addClass("panel-success");
+                $('.code-panel').removeClass("panel-primary").addClass("panel-success");
                 $('.code-panel .panel-body').append('<p id=\'code-msg\' class"text-danger">Operation successful</p>');
 
             },
             error: function (data) {
                 $('#code_form')[0].reset();
-                $('.code-panel').removeClass("panel-default").addClass("panel-danger");
+                $('.code-panel').removeClass("panel-primary").addClass("panel-danger");
                 $('.code-panel .panel-body').append('<p id="code-msg" class"text-danger">Uh oh, must have been a bad code</p>');
 
             },
